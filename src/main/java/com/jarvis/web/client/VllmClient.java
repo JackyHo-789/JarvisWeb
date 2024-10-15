c3af51b9109b56d80cd51701dd2f569a222ebfac
Jacky Ho <hocheuklung8@gmail.com>
2024-10-15 11:08:15 +0800

    update client - stream response

--- src/main/java/com/jarvis/web/client/VllmClient.java
+++ src/main/java/com/jarvis/web/client/VllmClient.java
@@ -1,20 +1,17 @@
 package com.jarvis.web.client;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jarvis.web.model.dto.ChatCompletionRequest;
-import com.jarvis.web.model.dto.ChatCompletionResponse;
-import com.jarvis.web.model.dto.ChatMessage;
+import com.jarvis.web.model.dto.*;
 import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 
 import java.io.BufferedReader;
@@ -25,13 +22,13 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
 @Component
 public class VllmClient {
 
-    public ResponseEntity<StreamingResponseBody> handleRequest(ChatCompletionRequest request) throws IOException {
+    public static ResponseBodyEmitter handleRequest(ChatCompletionRequest request) throws IOException {
         CloseableHttpClient client = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost("http://192.168.8.88:8000/v1/chat/completions");
         ObjectMapper mapper = new ObjectMapper();
@@ -41,30 +38,34 @@ public class VllmClient {
 
         httpPost.setEntity(stringEntity);
         httpPost.setHeader("Accept", "application/json");
-
-        try (CloseableHttpResponse response = client.execute(httpPost)) {
-            System.out.println(response);
-            final HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                try (InputStream inputStream = entity.getContent()) {
-                    return new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
-                            .lines()
-                            .map(line -> {
-                                if (!line.isEmpty() && !line.equals("data: [DONE]")) {
-                                    ChatCompletionResponse dto = mapLineToDTO(line);
-                                    assert dto != null; // 檢查dto是否為null
-//                                    System.out.println(dto.getChoices().getFirst().getDelta().getContent()); // 打印每一行
-                                    return dto;
-                                }
-                                return null;
-                            })
-                            .filter(Objects::nonNull) // 過濾掉null值
-                            .collect(Collectors.toList()); // 收集所有元素到一個列表中
+        ResponseBodyEmitter emitter = new ResponseBodyEmitter();
+        Executors.newSingleThreadExecutor().execute(() -> {
+            try (CloseableHttpResponse response = client.execute(httpPost)) {
+                final HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    try (InputStream inputStream = entity.getContent()) {
+                        Stream<List<ChatCompletionResponseChoice>> Result = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+                                .lines().map(line -> {
+                                    if (!line.isEmpty() && !line.equals("data: [DONE]")) {
+                                        ChatCompletionResponse dto = mapLineToDTO(line);
+                                        if (dto == null) {
+                                            throw new IllegalArgumentException("dto cannot be null");
+                                        }
+                                        return dto.getChoices();
+                                    }
+                                    return null;
+                                })
+                                .filter(Objects::nonNull);
+                        emitter.send(Result);
+                        emitter.complete();
+                    }
                 }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        }
+        });
         System.out.println("end");
-        return new ArrayList<>(); // 返回一個空的列表
+        return emitter; // 返回一個空的列表
     }
 
     private static ChatCompletionResponse mapLineToDTO(String line) {
@@ -80,7 +81,7 @@ public class VllmClient {
 
     public static void main(String[] args) throws IOException {
         ChatCompletionRequest request = new ChatCompletionRequest();
-        request.setMaxTokens(2000);
+        request.setMaxTokens(100);
         ChatMessage message = new ChatMessage();
         message.setRole("user");
         message.setContent("Write me 1000 words about elon musk");
@@ -93,6 +94,6 @@ public class VllmClient {
         request.setRepetition_penalty(1);
         request.setModel("Qwen/Qwen2.5-14B-Instruct-GPTQ-Int4");
         request.setStream(true);
-//        handleRequest(request);
+        System.out.println(handleRequest(request));
     }
 }
