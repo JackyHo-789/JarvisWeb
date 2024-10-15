5fba1137d5e2909319e14c6fb648657fd0b531be
Jacky Ho <hocheuklung8@gmail.com>
2024-10-15 11:38:40 +0800

    update dtos - add @JsonInclude

--- src/main/java/com/jarvis/web/model/dto/ChatCompletionResponse.java
+++ src/main/java/com/jarvis/web/model/dto/ChatCompletionResponse.java
@@ -1,11 +1,13 @@
 package com.jarvis.web.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ChatCompletionResponse {
     @JsonProperty("id")
     private String id;
